f830d8b0cbc59dfcde724c48062bd6b7f5a69737
amitsharma@nurturelabs.co <amitsharma0843@gmail.com>
2021-07-17 15:13:23 +0530

    changes

--- assignmentApis/settings.py
+++ assignmentApis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-by3^^y1%+tyh6mleo))e9ze()nmv!4e0h_(2e!ws9^_!c)sv6&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
