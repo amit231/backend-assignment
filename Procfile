e42c2579386b7714354257e1d4ead2d82cc08b3a
amitsharma@nurturelabs.co <amitsharma0843@gmail.com>
2021-07-17 15:02:07 +0530

    prod changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT assignmentApis.wsgi:application
--- assignmentApis/settings.py
+++ assignmentApis/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-by3^^y1%+tyh6mleo))e9ze()nmv!4e0h_(2e!ws9^_!c)sv6&'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["amitukhli.herokuapp.com", "localhost"]
 
 
 # Application definition
